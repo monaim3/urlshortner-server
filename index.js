f618be238455093d5b260f9c0589000166569c7e
monaim3 <monaimmukul75@gmail.com>
2024-06-15 22:21:39 +0600

    update

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const baseUrl = `http://localhost:3000/`;
 const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
 // Serve static files from the 'public' directory
-// app.use(express.static(path.join(__dirname, '..', 'Public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'Public', 'index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 app.get('/:shortUrl', (req, res) => {
   const shortUrl = req.params.shortUrl;
